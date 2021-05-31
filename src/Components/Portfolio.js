11abc96578822e307be41d89237ad9d561ea2430
Matthew Bardsley <Matthew.Bardsley@warwick.ac.uk>
2021-05-31 17:26:50 +0100

    Goes to link on click

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,19 +8,36 @@ class Portfolio extends Component {
       var posts = this.props.data.posts.map(function(posts){
         var postImage = 'images/portfolio/'+posts.id+'.png';
 	var links = posts.category.split(',');
+	if (links.length == 1 && links[0] != "") {
 	  return <div key={posts.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a id={posts.id} role="button" title={posts.caption.substr(0,100) + "..."}>
+            <a id={posts.id} role="button" title={posts.caption.substr(0,100) + "..."} href={links[0]} target="_blank">
                <img alt={posts.caption.substr(0,100) + "..."} src={postImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-		      <h5>{posts.caption.substr(0, 100)}...</h5>
+		      <h5>{posts.caption.substr(0,100)}...</h5>
 			<button id={posts.id} className="overlayButton"></button>
                   </div>
                 </div>
             </a>
           </div>
         </div>
+      }
+	else {
+		return <div key={posts.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a id={posts.id} role="button" title={posts.caption.substr(0,100) + "..."}>
+               <img alt={posts.caption.substr(0,100) + "..."} src={postImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+		      <h5>{posts.caption.substr(0,100)}...</h5>
+			<button id={posts.id} className="overlayButton"></button>
+                  </div>
+                </div>
+            </a>
+          </div>
+        </div>
+	}
       })
     }
 
